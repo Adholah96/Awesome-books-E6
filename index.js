8a6da32b565364448b9a7ec4dbe16395b9ed925d
Adholah96 <adholah96@gmail.com>
2023-02-21 16:44:24 +0300

    Add modules and import them

---
+++ index.js
@@ -0,0 +1,23 @@
+import setTime from './modules/currentTime.js'
+import { Library, libraryContainer } from './modules/mainclass.js'
+import singlePage from './modules/SinglePage.js'
+
+const form = document.getElementById('myForm')
+
+// innitialize form submit to create Library instance
+form.addEventListener('submit', (e) => {
+  e.preventDefault()
+  const id = Math.floor(Math.random() * 1000)
+  const book = new Library(id, authorName.value, titleName.value)
+  libraryContainer.push(book)
+  Library.displayBooks()
+  Library.clearInput()
+  Library.deleteBook()
+  Library.addLocalStorage(libraryContainer)
+})
+window.addEventListener('DOMContentLoaded', () => {
+  Library.displayBooks()
+  Library.deleteBook()
+})
+setTime()
+singlePage()
